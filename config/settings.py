33c7847bad7ab0ab0c7dc537b9b6ffecf3933d38
admin <admin@sinc.jp>
2022-05-25 11:19:32 +0900

    Changed the DATABASES for the site.

--- config/settings.py
+++ config/settings.py
@@ -75,11 +75,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'TestDjango',
+        'USER': 'sa',
+        'PASSWORD': '',
+       'HOST': '192.168.10.22\SQL2017',
+
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
+    },
 }
 
 # Password validation
